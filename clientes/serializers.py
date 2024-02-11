3172e46472dc928216d958ebb3f25f28e2c872b5
Paulo Matheus Ferrari <paulomferrari26@gmail.com>
2024-02-11 09:08:58 -0300

    Aula 5

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -8,12 +8,12 @@ class ClienteSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({'cpf':'O CPF deve conter 11 dígitos'})        
+            raise serializers.ValidationError({'cpf':'Número de CPF inválido'})        
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome':'O nome não pode conter dígitos'})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg':'O RG deve conter 9 dígitos'})
         if not celular_valido(data['celular']):
-            raise serializers.ValidationError({'celular':'O celular deve ter 11 dígitos'})
+            raise serializers.ValidationError({'celular':'O número de celular deve seguir esse modelo: xx xxxxx-xxxx'})
         return data
     
